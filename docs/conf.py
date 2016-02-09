4881811ce621b8e2fbbad71d97d83303dbf1399b
Pavlo Dyban <pdyban@gmail.com>
2016-02-09 16:22:57 +0100

    Added mocks for python modules to readthedocs conf

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,15 @@ import shlex
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['skimage', 'matplotlib', 'scipy', 'numpy',]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
